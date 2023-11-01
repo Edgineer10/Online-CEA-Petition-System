6172629745c6851a1737cbc5e2fa1d7cb542d7e6
Edgineer10 <redlilan15@gmail.com>
2023-11-01 11:49:24 +0900

    11/1

---
+++ models/Course.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+
+    courseProg: {
+        type: String,
+        required: true
+    },
+
+    currYear: {
+        type: int,
+        required: true
+    },
+
+    courseNumber: {
+        type: String,
+        required: true
+    },
+    DescTitle: {
+        type: String,
+        required: true
+    },
+    Units: {
+        type: int,
+        required: true
+    },
+    courseYear: {
+        type: int,
+        required: true
+    },
+    courseSem: {
+        type: int,
+        required: true
+    },
+    active: {
+        type: Boolean,
+        default: true
+    }
+})
+
+module.exports = mongoose.model('Course', userSchema)
---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    idNumber: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    middleName: {
+        type: String,
+        required: true
+    },
+    birthday: {
+        type: Date,
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    active: {
+        type: Boolean,
+        default: true
+    }
+})
+
+module.exports = mongoose.model('User', userSchema)
